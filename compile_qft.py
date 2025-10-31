a2b5e7d637c1c50376122674ed49ce8d2c164a92
Mathias Weiden <mtweiden@berkeley.edu>
2025-10-31 11:31:57 -0700

    specify precision and num_qubits

--- compile_qft.py
+++ compile_qft.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import numpy as np
 
 from bqskit.ir import Circuit
@@ -13,37 +14,50 @@ def qft(num_qubits: int) -> np.ndarray:
     x = np.fromfunction(lambda x, y: root**(x * y), (n, n))
     return np.array(x) / np.sqrt(n)
 
-u = qft(num_qubits=2)
-circuit = Circuit.from_unitary(u)
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('precision', type=int, help='Precision for synthesis.')
+    parser.add_argument(
+        '--num_qubits', type=int, help='Number of qubits for QFT.', default=2
+    )
+    args = parser.parse_args()
 
-# Let's choose a precision for synthesis
-precision = 8
-_precision = 10 ** (-precision)
+    num_qubits = args.num_qubits
+    u = qft(num_qubits)
+    circuit = Circuit.from_unitary(u)
 
-# Regular syntheis
-reg_circuit = compile(circuit, synthesis_epsilon=_precision)
-# Let's save the regular qasm
-reg_circuit.save(f'outputs/reg_qft_{precision}.qasm')
+    # Let's choose a precision for synthesis
+    precision = args.precision
+    _precision = 10 ** (-precision)
+    print(
+        f'Starting synthesis for QFT(num_qubits={num_qubits}) '
+        f'with precision 10^-{precision}...'
+    )
 
-# Fault-tolerant synthesis
-machine = CliffordTModel(circuit.num_qudits)
-ft_circuit = compile(circuit, model=machine)
-# Let's save the fault-tolerant qasm
-ft_circuit.save(f'outputs/ft_qft_{precision}.qasm')
+    # Regular syntheis
+    reg_circuit = compile(circuit, synthesis_epsilon=_precision)
+    # Let's save the regular qasm
+    reg_circuit.save(f'outputs/reg_qft_{precision}.qasm')
 
-print('The Clifford+T gate set:')
-print(clifford_t_gates)
-print('The regular circuit contains these gates:')
-print(reg_circuit.gate_set)
-print('The fault-tolerant circuit contains these gates:')
-print(ft_circuit.gate_set)
+    # Fault-tolerant synthesis
+    machine = CliffordTModel(circuit.num_qudits)
+    ft_circuit = compile(circuit, model=machine)
+    # Let's save the fault-tolerant qasm
+    ft_circuit.save(f'outputs/ft_qft_{precision}.qasm')
 
-# Let's look at how far each circuit is from the target unitary
-reg_unitary = reg_circuit.get_unitary()
-ft_unitary = ft_circuit.get_unitary()
+    print('The Clifford+T gate set:')
+    print(clifford_t_gates)
+    print('The regular circuit contains these gates:')
+    print(reg_circuit.gate_set)
+    print('The fault-tolerant circuit contains these gates:')
+    print(ft_circuit.gate_set)
 
-reg_d = reg_unitary.get_distance_from(u)
-ft_d = ft_unitary.get_distance_from(u)
+    # Let's look at how far each circuit is from the target unitary
+    reg_unitary = reg_circuit.get_unitary()
+    ft_unitary = ft_circuit.get_unitary()
 
-print(f'Regular circuit distance from target unitary: {reg_d}')
-print(f'Fault-tolerant circuit distance from target unitary: {ft_d}')
+    reg_d = reg_unitary.get_distance_from(u)
+    ft_d = ft_unitary.get_distance_from(u)
+
+    print(f'Regular circuit distance from target unitary: {reg_d}')
+    print(f'Fault-tolerant circuit distance from target unitary: {ft_d}')
